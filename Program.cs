2261be88a24c0ee063709e52f5b4c90eb3e59e62
Abdurabd <49178533+Abdurabd@users.noreply.github.com>
2023-12-03 22:03:46 +0000

    Refactor challenges 0.1

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using BenchmarkDotNet.Running;
 using Dotnet_Interview.BenchMark;
 using Dotnet_Interview.Interview;
+using Dotnet_Interview.Refactoring;
 
 namespace Dotnet_Interview;
 
@@ -12,6 +13,20 @@ class Program
         //BenchmarkRunner.Run<LinkedListAddBenchMark>();
         //BenchmarkRunner.Run<EncryptionBenchMark>();
         //BenchmarkRunner.Run<LinqBenchmark>();
-        BenchmarkRunner.Run<StringBuilderBenchMark>();
+        //BenchmarkRunner.Run<StringBuilderBenchMark>();
+
+        //Refactor
+
+        //BaggageCalculator
+        int numChecked = 2;
+        int numCarryOn = 1;
+        int numPassengers = 2;
+
+        BaggageCalculator baggageCalculator
+             = new();
+        DateTime travelTime = DateTime.Now;
+        decimal price = baggageCalculator.CalculatePrice(numChecked, numCarryOn, numPassengers, numPassengers, travelTime);
+
+        Console.WriteLine($"{numChecked} checked and {numCarryOn} carry-on bags for {numPassengers} passengers is {price:C}");
     }
 }
---
+++ Refactoring/BaggageCalculator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet_Interview.Refactoring
+{
+    public class BaggageCalculator
+    {
+        private decimal holidayFeePercent = 0.1m;
+        public decimal HolidayFeePercent
+        {
+            get { return holidayFeePercent; } 
+            set { holidayFeePercent = value; }
+        }
+        
+        public decimal CalculatePrice(int bags, int carryOn, int passengers, DateTime travelTime)
+        {
+            decimal total = 0;
+
+            if (carryOn > 0)
+            {
+                Console.WriteLine($"Carry-on: {carryOn * 30M}");
+                total += carryOn * 30M;
+            }
+
+            if (bags > 0)
+            {
+                if (bags <= passengers)
+                {
+                    Console.WriteLine($"Carry-on: {bags * 40M}");
+                    total += bags * 40M;
+                }
+
+                else
+                {
+                    decimal checkedFee = (passengers * 40M) + ((bags - passengers) * 50M);
+
+                    Console.WriteLine($"Checked: {checkedFee}");
+                    total += checkedFee;
+                }
+            }
+
+            if(travelTime.Month >=11 || travelTime.Month <= 2) 
+            {
+                Console.WriteLine("Holiday Fee: " + (total * HolidayFeePercent));
+                total += total * HolidayFeePercent;
+            }
+
+            return total;
+        }
+
+        private decimal CalculatePriceFlat(int numBags)
+        {
+            decimal total = 0;
+
+            return 100M;
+
+            return numBags * 50M;
+        }
+
+    }
+}
